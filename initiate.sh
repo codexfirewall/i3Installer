3d339d8f460d5a95ee179f8d84d7cddcbf3ca4de
Ssp <satyapratheeshxe@gmail.com>
2024-12-29 13:44:56 +0530

    first commit

---
+++ initiate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash 
+
+#run as sudo
+
+cd ~
+sudo apt install nala
+sudo nala install git
+
+mkdir i3Install
+cd i3Install/
+
+git clone https://github.com/codexfirewall/dotfiles.git
+
+
